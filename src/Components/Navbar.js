30820599046e012cbd515ec2f60a66fe45892ecd
Gianni Versées <verseesgianni@outlook.com>
2022-06-19 18:01:59 +0200

    conditional showing of home in nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,9 +6,10 @@ import NavDropDown from 'react-bootstrap/NavDropdown'
 
 function Navbar() {
   return (
-    <div className="App">
+    <>
+      {console.log(window.location.href)}
       <div className='brand'>
-      <img src={process.env.PUBLIC_URL + '/gve.png'} alt='logo' width="619px" height="186px" />
+        <img src={process.env.PUBLIC_URL + '/gve.png'} alt='logo' width="619px" height="186px" />
       </div>
       <NavBar bg='dark' variant='dark'
         sticky='top' expand='sm' collapseOnSelect className='justify-content-center'>{/* bg kan verwijzing naar cssklasse zijn ook. kleur navbar. Sticky= vast aan bovenkant bij scroll? */}
@@ -16,6 +17,11 @@ function Navbar() {
 
         <NavBar.Collapse className="justify-content-center ">
           <Nav>
+
+            {window.location.href.includes("/Home") ? null :
+              <Nav.Link href="Home">Home</Nav.Link>
+            }
+
             <Nav.Link href="About">About me</Nav.Link>
             <Nav.Link href="Education">Education</Nav.Link>
 
@@ -28,10 +34,7 @@ function Navbar() {
           </Nav>
         </NavBar.Collapse>
       </NavBar>
-      <div className='content'>
-        this is content 4.
-      </div>
-    </div>
+    </>
   );
 }
 
