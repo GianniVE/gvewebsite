e14b2587943780444fb3967c1d6b483dc8efadc4
Gianni Versées <verseesgianni@outlook.com>
2022-06-19 15:32:52 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import logo from './gve.png'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import NavBar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropDown from 'react-bootstrap/NavDropdown'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='brand'>
+      <img src={logo} alt='logo' width="619px" height="186px"/>
+      </div>
+      <NavBar bg='dark' variant='dark'
+        sticky='top' expand='sm' collapseOnSelect className='justify-content-center'>{/* bg kan verwijzing naar cssklasse zijn ook. kleur navbar. Sticky= vast aan bovenkant bij scroll? */}
+        <NavBar.Toggle />
+
+        <NavBar.Collapse className="justify-content-center">
+          <Nav>
+            <Nav.Link href="About">About me</Nav.Link>
+            <Nav.Link href="Education">Education</Nav.Link>
+
+            <NavDropDown title="Projects">
+              <NavDropDown.Item href="Proj1">Proj1</NavDropDown.Item>
+              <NavDropDown.Item href="Proj2">Proj2</NavDropDown.Item>
+              <NavDropDown.Item href="Proj3">Proj3</NavDropDown.Item>
+            </NavDropDown>
+
+          </Nav>
+        </NavBar.Collapse>
+      </NavBar>
+      <div className='content'>
+        this is content 2.
+      </div>
     </div>
   );
 }
