835e7a0182e19b1cf32124d1cc97104a6b33bec0
Gianni Versées <verseesgianni@outlook.com>
2022-06-21 23:31:11 +0200

    about content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,13 +7,21 @@ function About() {
             <div class="cards_img">
                 <img src={process.env.PUBLIC_URL + '/gianni.jpg'} alt='logo' className='image'></img>
             </div>
-            <div class="card">
+            <div class="card parent">
                 Hi!
-                <br />
-                My name is Gianni.
-                <br />
-                I'm a second year computer science Student
-                at the Karel de Grote Hogeschool (specialisation Full-Stack).
+            
+                My name is Gianni Versées.
+                <br /><br/>
+                I'm a second year computer science Student at the Karel de Grote Hogeschool (specialisation Full-Stack).
+                <br/><br/>
+                In my free time i'm a volleyball player/coach.
+                <br/><br/>
+                Wanna know more about me? Take a look at my curriculum vitae.
+                <br/><br/>
+
+                <div className='cv'>
+                    <a href={process.env.PUBLIC_URL + '/cv_gianni.pdf'} target='blank'>Gianni Versées - C.V.</a>
+                </div> 
             </div>
         </div>
     );
